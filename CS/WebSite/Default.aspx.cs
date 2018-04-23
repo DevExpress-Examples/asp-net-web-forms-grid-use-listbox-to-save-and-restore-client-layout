2995fa5003a4796bd73a3c37a1bb69eb40b2d868
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxGridView;
+using DevExpress.Web.ASPxClasses;
+
+public partial class _Default : System.Web.UI.Page {
+    bool isSaveLayout = true;
+
+    protected void Page_Load(object sender, EventArgs e) {
+        Grid.JSProperties["cpCallback"] = 0;
+        if (Session["layout"] == null) {
+            Session["layout"] = new Dictionary<string, string>();
+        }
+        else {
+            Dictionary<string, string> dictionary = Session["layout"] as Dictionary<string, string>;
+            ListBox.Items.Clear();
+            foreach (KeyValuePair<string, string> item in dictionary) {
+                ListEditItem editItem = new ListEditItem(item.Key, item.Value);
+                ListBox.Items.Add(editItem);
+            }
+        }
+    }
+    protected void ListBox_Callback(object sender, CallbackEventArgsBase e) {
+        if (Session["selectedLayout"] != null)
+            ListBox.JSProperties["cpSelected"] = ListBox.Items.IndexOfValue(Session["selectedLayout"]);
+    }
+    protected void Grid_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        if (ListBox.SelectedItem != null) {
+            Grid.LoadClientLayout(ListBox.SelectedItem.Value.ToString());
+            isSaveLayout = false;
+            Grid.JSProperties["cpCallback"] = 1;
+        }
+    }
+    protected void Grid_ClientLayout(object sender, ASPxClientLayoutArgs e) {
+        if (e.LayoutMode == ClientLayoutMode.Saving && isSaveLayout) {
+            string key = DateTime.Now.ToString();
+            string layout = e.LayoutData;
+            Dictionary<string, string> dictionary = Session["layout"] as Dictionary<string, string>;
+            if (!dictionary.ContainsValue(layout)) {
+                ListBox.Items.Add(key, layout);
+                dictionary[key] = layout;
+            }
+            else {
+                Session["selectedLayout"] = layout;
+            }
+            Session["layout"] = dictionary;
+        }
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,54 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxClasses
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private isSaveLayout As Boolean = True
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Grid.JSProperties("cpCallback") = 0
+		If Session("layout") Is Nothing Then
+			Session("layout") = New Dictionary(Of String, String)()
+		Else
+			Dim dictionary As Dictionary(Of String, String) = TryCast(Session("layout"), Dictionary(Of String, String))
+			ListBox.Items.Clear()
+			For Each item As KeyValuePair(Of String, String) In dictionary
+				Dim editItem As New ListEditItem(item.Key, item.Value)
+				ListBox.Items.Add(editItem)
+			Next item
+		End If
+	End Sub
+	Protected Sub ListBox_Callback(ByVal sender As Object, ByVal e As CallbackEventArgsBase)
+		If Session("selectedLayout") IsNot Nothing Then
+			ListBox.JSProperties("cpSelected") = ListBox.Items.IndexOfValue(Session("selectedLayout"))
+		End If
+	End Sub
+	Protected Sub Grid_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+		If ListBox.SelectedItem IsNot Nothing Then
+			Grid.LoadClientLayout(ListBox.SelectedItem.Value.ToString())
+			isSaveLayout = False
+			Grid.JSProperties("cpCallback") = 1
+		End If
+	End Sub
+	Protected Sub Grid_ClientLayout(ByVal sender As Object, ByVal e As ASPxClientLayoutArgs)
+		If e.LayoutMode = ClientLayoutMode.Saving AndAlso isSaveLayout Then
+			Dim key As String = DateTime.Now.ToString()
+			Dim layout As String = e.LayoutData
+			Dim dictionary As Dictionary(Of String, String) = TryCast(Session("layout"), Dictionary(Of String, String))
+			If (Not dictionary.ContainsValue(layout)) Then
+				ListBox.Items.Add(key, layout)
+				dictionary(key) = layout
+			Else
+				Session("selectedLayout") = layout
+			End If
+			Session("layout") = dictionary
+		End If
+	End Sub
+End Class
